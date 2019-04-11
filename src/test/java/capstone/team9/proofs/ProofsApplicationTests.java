29318b07e4bd0640cf04f3d813f2e3adc7876ac7
Jay Arellano <jaarellano@csumb.edu>
2019-04-11 11:23:06 -0700

    first commit

---
+++ src/test/java/capstone/team9/proofs/ProofsApplicationTests.java
@@ -0,0 +1,16 @@
+package capstone.team9.proofs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProofsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
