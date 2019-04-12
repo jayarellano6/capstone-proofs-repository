78936bf0a99918088e21e589bc3563ea04243de1
Jay Arellano <jaarellano@csumb.edu>
2019-04-12 14:24:45 -0700

    small change

--- src/main/java/capstone/team9/proof/ProofsApplication.java
+++ src/main/java/capstone/team9/proof/ProofsApplication.java
@@ -2,9 +2,11 @@ package capstone.team9.proof;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class ProofsApplication{
 
 	public static void main(String[] args) {
