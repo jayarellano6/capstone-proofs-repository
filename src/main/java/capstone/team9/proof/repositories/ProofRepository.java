bdbe6c950eba8d41aee9a3f70581a38a3272cfb7
Jay Arellano <jaarellano@csumb.edu>
2019-04-16 12:20:11 -0700

    change

--- src/main/java/capstone/team9/proof/repositories/ProofRepository.java
+++ src/main/java/capstone/team9/proof/repositories/ProofRepository.java
@@ -2,14 +2,9 @@ package capstone.team9.proof.repositories;
 
 import capstone.team9.proof.models.Proofs;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProofRepository extends MongoRepository<Proofs, String> {
-    @Query(value = "{'id':?0}")
-    Proofs findByRepoId(String id);
-
-    @Query (value = "{'username.username':?0}")
-    Proofs findByUsername(String username);
+    Proofs findByIdEquals(String id);
 }
