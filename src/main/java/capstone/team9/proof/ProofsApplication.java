224b34546681aa527993a1673816de4624c229d3
Jay Arellano <jaarellano@csumb.edu>
2019-04-16 12:22:32 -0700

    change

--- src/main/java/capstone/team9/proof/ProofsApplication.java
+++ src/main/java/capstone/team9/proof/ProofsApplication.java
@@ -3,6 +3,7 @@ package capstone.team9.proof;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ProofsApplication{
 
